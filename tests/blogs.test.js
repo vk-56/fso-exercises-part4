384453b145149ee977d2e3efcc6f2f778e8cd5ae
VK <56174314+vk-56@users.noreply.github.com>
2023-11-12 23:40:10 +0530

    Finished till 4.8

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,164 +1,33 @@
-const listHelper = require('../utils/list_helper')
+const supertest = require('supertest')
+//const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
 
-test('dummy returns one', () => {
-    const blogs = []
+// Used for making HTTP requests to the backend.
+const api = supertest(app)
 
-    const result = listHelper.dummy(blogs)
-    expect(result).toBe(1)
-})
+const Blog = require('../models/blog')
 
-describe('total likes', () => {
-    test('of empty list is zero', () => {
-        const listWithNoBlog = []
-        const result = listHelper.totalLikes(listWithNoBlog)
-        expect(result).toBe(0)
-    })
+beforeEach(async () => {
+    await Blog.deleteMany({})
 
-    test('when list has only one blog equals the likes of that', () => {
-        const listWithOneBlog = [
-            {
-                _id: '5a422a851b54a676234d17f7',
-                title: 'React patterns',
-                author: 'Michael Chan',
-                url: 'https://reactpatterns.com/',
-                likes: 7,
-                __v: 0
-            }
-        ]
-        const result = listHelper.totalLikes(listWithOneBlog)
-        expect(result).toBe(7)
-    })
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+}, 200000)
 
-    test('of a bigger list is calculated right', () => {
-        const listWithBlogs = [
-            {
-                _id: '5a422a851b54a676234d17f7',
-                title: 'React patterns',
-                author: 'Michael Chan',
-                url: 'https://reactpatterns.com/',
-                likes: 7,
-                __v: 0
-            },
-            {
-                _id: '5a422aa71b54a676234d17f8',
-                title: 'Go To Statement Considered Harmful',
-                author: 'Edsger W. Dijkstra',
-                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-                likes: 5,
-                __v: 0
-            },
-            {
-                _id: '5a422b3a1b54a676234d17f9',
-                title: 'Canonical string reduction',
-                author: 'Edsger W. Dijkstra',
-                url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-                likes: 12,
-                __v: 0
-            },
-            {
-                _id: '5a422b891b54a676234d17fa',
-                title: 'First class tests',
-                author: 'Robert C. Martin',
-                url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-                likes: 10,
-                __v: 0
-            },
-            {
-                _id: '5a422ba71b54a676234d17fb',
-                title: 'TDD harms architecture',
-                author: 'Robert C. Martin',
-                url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-                likes: 0,
-                __v: 0
-            },
-            {
-                _id: '5a422bc61b54a676234d17fc',
-                title: 'Type wars',
-                author: 'Robert C. Martin',
-                url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-                likes: 2,
-                __v: 0
-            }
-        ]
-        const result = listHelper.totalLikes(listWithBlogs)
-        expect(result).toBe(36)
-    })
-})
+describe('when there are some blogs saved', () => {
+    test('blogs are returned as JSON', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    }, 200000)
 
-describe('favorite blog', () => {
-    test('of empty list is empty object', () => {
-        const listWithNoBlog = []
-        const result = listHelper.favoriteBlog(listWithNoBlog)
-        expect(result).toEqual({})
-    })
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
 
-    test('when list has only one blog is the blog itself', () => {
-        const listWithOneBlog = [
-            {
-                _id: '5a422a851b54a676234d17f7',
-                title: 'React patterns',
-                author: 'Michael Chan',
-                url: 'https://reactpatterns.com/',
-                likes: 7,
-                __v: 0
-            }
-        ]
-        const result = listHelper.favoriteBlog(listWithOneBlog)
-        expect(result).toEqual(listWithOneBlog[0])
-    })
-
-    test('of a bigger list is calculated right', () => {
-        const listWithBlogs = [
-            {
-                _id: '5a422a851b54a676234d17f7',
-                title: 'React patterns',
-                author: 'Michael Chan',
-                url: 'https://reactpatterns.com/',
-                likes: 7,
-                __v: 0
-            },
-            {
-                _id: '5a422aa71b54a676234d17f8',
-                title: 'Go To Statement Considered Harmful',
-                author: 'Edsger W. Dijkstra',
-                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-                likes: 5,
-                __v: 0
-            },
-            {
-                _id: '5a422b3a1b54a676234d17f9',
-                title: 'Canonical string reduction',
-                author: 'Edsger W. Dijkstra',
-                url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-                likes: 12,
-                __v: 0
-            },
-            {
-                _id: '5a422b891b54a676234d17fa',
-                title: 'First class tests',
-                author: 'Robert C. Martin',
-                url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-                likes: 10,
-                __v: 0
-            },
-            {
-                _id: '5a422ba71b54a676234d17fb',
-                title: 'TDD harms architecture',
-                author: 'Robert C. Martin',
-                url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-                likes: 0,
-                __v: 0
-            },
-            {
-                _id: '5a422bc61b54a676234d17fc',
-                title: 'Type wars',
-                author: 'Robert C. Martin',
-                url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-                likes: 2,
-                __v: 0
-            }
-        ]
-        const result = listHelper.favoriteBlog(listWithBlogs)
-        expect(result).toEqual(listWithBlogs[2])
-    })
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    }, 200000)
 })
